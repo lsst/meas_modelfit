89f6be83fc57085bc76655b107dbceb5f3bb5ac7
Jim Bosch <jbosch@astro.princeton.edu>
2014-09-10 14:08:42 -0400

    Rework still-failing test for PSF fitting

--- tests/testPsfFitterApply.py
+++ tests/testPsfFitterApply.py
@@ -25,72 +25,68 @@
 import unittest
 import os
 import numpy
+import glob
+
 import lsst.utils.tests
 import lsst.shapelet
 import lsst.afw.geom.ellipses
 import lsst.meas.multifit
 import lsst.afw.math
 import lsst.afw.image
+import lsst.meas.base
+
+import lsst.pex.logging
+lsst.pex.logging.Debug("meas.multifit.optimizer", 10)
 
 NUMBER_OF_TEST_KERNELS = 2;
 DATA_DIR = os.path.join(os.environ["MEAS_MULTIFIT_DIR"], "tests", "data")
 
+def computeMoments(image):
+    """Helper function to compute moments of a postage stamp about its origin."""
+    maskedImage = lsst.afw.image.makeMaskedImage(image)
+    result = lsst.meas.base.SdssShapeAlgorithm.Result()
+    lsst.meas.base.SdssShapeAlgorithm.apply(
+        maskedImage,
+        lsst.afw.detection.Footprint(image.getBBox(lsst.afw.image.PARENT)),
+        lsst.afw.geom.Point2D(0.0, 0.0),
+        result
+        )
+    return result.getShape()
+
 class PsfFitterApplyTestCase(lsst.utils.tests.TestCase):
     def setUp(self):
         """Build a dict of configs for testing.
-        This was directly cut and pasted from testPsfFitter. 
-        I will either extend these here, or add the testApply to testPsfFitter.py.
         """
         self.configs = {}
-        self.configs['fixed'] = lsst.meas.multifit.PsfFitterConfig()
         self.configs['ellipse'] = lsst.meas.multifit.PsfFitterConfig()
         self.configs['ellipse'].primary.ellipticityPriorSigma = 0.3
         self.configs['ellipse'].primary.ellipticityPriorSigma = 0.3
         self.configs['ellipse'].primary.radiusPriorSigma = 0.5
-        self.configs['ellipse'].wings.ellipticityPriorSigma = 0.3
-        self.configs['ellipse'].wings.ellipticityPriorSigma = 0.3
-        self.configs['ellipse'].wings.radiusPriorSigma = 0.5
-        self.configs['full'] = lsst.meas.multifit.PsfFitterConfig()
-        self.configs['full'].inner.order = 0
-        self.configs['full'].primary.order = 4
-        self.configs['full'].wings.order = 4
-        self.configs['full'].outer.order = 0
-        self.configs['full'].inner.ellipticityPriorSigma = 0.3
-        self.configs['full'].inner.ellipticityPriorSigma = 0.3
-        self.configs['full'].inner.radiusPriorSigma = 0.5
-        self.configs['full'].inner.positionPriorSigma = 0.1
-        self.configs['full'].primary.ellipticityPriorSigma = 0.3
-        self.configs['full'].primary.ellipticityPriorSigma = 0.3
-        self.configs['full'].primary.radiusPriorSigma = 0.5
-        self.configs['full'].primary.positionPriorSigma = 0.1
-        self.configs['full'].wings.ellipticityPriorSigma = 0.3
-        self.configs['full'].wings.ellipticityPriorSigma = 0.3
-        self.configs['full'].wings.radiusPriorSigma = 0.5
-        self.configs['full'].wings.positionPriorSigma = 0.1
-        self.configs['full'].outer.ellipticityPriorSigma = 0.3
-        self.configs['full'].outer.ellipticityPriorSigma = 0.3
-        self.configs['full'].outer.radiusPriorSigma = 0.5
-        self.configs['full'].outer.positionPriorSigma = 0.1
-        self.sctrl = lsst.afw.math.StatisticsControl()
+        self.configs['ellipse'].wings.order = 0
+        self.configs['ellipse'].wings.ellipticityPriorSigma = 0.0
+        self.configs['ellipse'].wings.ellipticityPriorSigma = 0.0
+        self.configs['ellipse'].wings.radiusPriorSigma = 0.0
 
     def tearDown(self):
         del self.configs
 
     def testApply(self):
-        for idxKernel in range(NUMBER_OF_TEST_KERNELS):
-            kernel = lsst.meas.algorithms.KernelPsfPersistableFacade_readFits(
-                         os.path.join(DATA_DIR, 'psfKernel%i.fits'%(idxKernel)))
-            kernelImage = kernel.computeKernelImage().convertF()
+        for filename in glob.glob(os.path.join(DATA_DIR, "psfs", "*.fits")):
+            kernelImageD = lsst.afw.image.ImageD(filename)
+            kernelImageF = kernelImageD.convertF()
+            shape = computeMoments(kernelImageF)
             for configKey in self.configs:
                 fitter = lsst.meas.multifit.PsfFitter(self.configs[configKey].makeControl())
-                multiShapeletFit = fitter.apply(kernelImage,
-                                                0.05,  #not sure what to put here
-                                                kernel.computeShape())
-                residualImage = -lsst.afw.image.ImageF(kernelImage)
-                multiShapeletFit.evaluate().addToImage(residualImage)
-                self.assertClose(0.0, lsst.afw.math.makeStatistics(residualImage,
-                                                                   lsst.afw.math.MEANSQUARE,
-                                                                   self.sctr).getValue())
+                multiShapeletFit = fitter.apply(kernelImageF,
+                                                0.1,  #not sure what to put here
+                                                shape)
+                print "Moments:", shape
+                for element in multiShapeletFit.getElements():
+                    print element.getEllipse().getCore()
+                modelImageD = lsst.afw.image.ImageD(kernelImageD.getBBox(lsst.afw.image.PARENT))
+                multiShapeletFit.evaluate().addToImage(modelImageD)
+                modelImageF = modelImageD.convertF()
+                self.assertClose(kernelImageD.getArray(), modelImageD.getArray(), plotOnFailure=True)
 
 def suite():
     """Returns a suite containing all the test cases in this module."""
