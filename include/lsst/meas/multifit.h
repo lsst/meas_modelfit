d4e34e944b1ec152020d797c06a0fee0e0af5e2f
Jim Bosch <jbosch@astro.princeton.edu>
2014-09-26 13:00:36 -0400

    Snapshot from HSC fork of CModel classes, as of tag HSC-3.2.1
    This commit is not expected to build, as it depends on
    meas_extensions_multiShapelet, and we've already removed that from the .cfg.

--- include/lsst/meas/multifit.h
+++ include/lsst/meas/multifit.h
@@ -44,5 +44,6 @@
 #include "lsst/meas/multifit/Mixture.h"
 #include "lsst/meas/multifit/optimizer.h"
 #include "lsst/meas/multifit/psf.h"
+#include "lsst/meas/multifit/CModel.h"
 
 #endif // !LSST_MEAS_MULTIFIT_H
