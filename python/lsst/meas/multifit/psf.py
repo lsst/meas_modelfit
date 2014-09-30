3fd3aac16e3312f435938854432647bfe4331d06
Jim Bosch <jbosch@astro.princeton.edu>
2014-09-30 17:31:03 -0400

    Add measurement plugins for approximating the PSF with shapelets

--- python/lsst/meas/multifit/psf.py
+++ python/lsst/meas/multifit/psf.py
@@ -25,15 +25,13 @@ import lsst.pex.config
 import lsst.meas.base
 from . import multifitLib
 
-class PsfFitterBasePluginConfig(lsst.meas.base.BasePluginConfig):
-    models = lsst.pex.config.ConfigDictDict(
-        dtype=multifitLib.PsfFitterConfig,
+
+class ShapeletPsfApproxConfig(lsst.pex.config.Config):
+    models = lsst.pex.config.ConfigDictField(
+        keytype=str,
+        itemtype=multifitLib.PsfFitterConfig,
         doc="a dictionary of models that can be used to fit the PSF",
-        default={
-            "SingleGaussian": multifitLib.PsfFitterConfig()
-            "DoubleGaussian": multifitLib.PsfFitterConfig(),
-            "Full": multifitLib.PsfFitterConfig()
-            }
+        default={} # populated in setDefaults; can't do it on a single line here
         )
     sequence = lsst.pex.config.ListField(
         dtype=str,
@@ -42,19 +40,113 @@ class PsfFitterBasePluginConfig(lsst.meas.base.BasePluginConfig):
         )
 
     def setDefaults(self):
-        super(PsfFitterPluginConfig, self).setDefaults()
+        super(ShapeletPsfApproxConfig, self).setDefaults()
+        self.models["SingleGaussian"] = multifitLib.PsfFitterConfig()
         self.models["SingleGaussian"].inner.order = -1
         self.models["SingleGaussian"].primary.order = 0
         self.models["SingleGaussian"].wings.order = -1
         self.models["SingleGaussian"].outer.order = -1
+        self.models["DoubleGaussian"] = multifitLib.PsfFitterConfig()
         self.models["DoubleGaussian"].inner.order = -1
         self.models["DoubleGaussian"].primary.order = 0
         self.models["DoubleGaussian"].wings.order = 0
         self.models["DoubleGaussian"].outer.order = -1
+        self.models["Full"] = multifitLib.PsfFitterConfig()
         self.models["Full"].inner.order = 0
         self.models["Full"].primary.order = 4
         self.models["Full"].wings.order = 4
         self.models["Full"].outer.order = 0
 
-class PsfFitterSingleFramePluginConfig(lsst.meas.base.SingleFramePluginConfig, PsfFitterBasePluginConfig):
+    def validate(self):
+        super(ShapeletPsfApproxConfig, self).validate()
+        if len(self.sequence) < 1:
+            raise ValueError("sequence must have at least one element")
+        for m in self.sequence:
+            if m not in self.models:
+                raise KeyError("All elements in sequence must be keys in models dict")
+
+class ShapeletPsfApproxMixin(object):
+    """Mixin base class for fitting shapelet approximations to the PSF model
+
+    This class does almost all of the work for its two derived classes, ShapeletPsfApproxSingleFramePlugin
+    and ShapeletPsfApproxForcedPlugin, which simply adapt it to the slightly different interfaces for
+    single-frame and forced measurement.  It in turn delegates its work to the C++ PsfFitter class;
+    it holds sequence of these corresponding to different models (generally with increasing complexity).
+    Each PsfFitter starts with the result of the previous one as an input, using PsfFitter::adapt to
+    hopefully allow these previous fits to reduce the time spent on the next one.
+
+    At present, this plugin does not define any failure flags, which will almost certainly
+    have to be changed in the future.  So far, however, I haven't actually seen it fail on
+    any PSFs I've given it, so I'll wait until we can run on large enough data volumes to
+    see what the actual failure modes are, instead of trying to guess them in advance.
+    """
+
+    def __init__(self, config, name, schema):
+        """Initialize the plugin, creating a sequence of PsfFitter instances to do the fitting and
+        MultiShapeletFunctionKey instances to save the results to a record.
+        """
+        self.sequence = []
+        for m in config.sequence:
+            fitter = multifitLib.PsfFitter(config.models[m].makeControl())
+            key = fitter.addFields(schema, schema[name][m].getPrefix())
+            self.sequence.append((fitter, key))
+
+    def measure(self, measRecord, exposure):
+        """Fit the configured sequence of models the given Exposure's Psf, as evaluated at
+        measRecord.getCentroid(), then save the results to measRecord.
+        """
+        if not exposure.hasPsf():
+            raise lsst.meas.base.FatalAlgorithmError("ShapeletPsfApprox requires Exposure to have a Psf")
+        psf = exposure.getPsf()
+        psfImage = psf.computeImage(measRecord.getCentroid())
+        psfShape = psf.computeShape(measRecord.getCentroid())
+        # Fit the first element in the sequence, using the PSFs moments to initialize the parameters
+        fitter, key = self.sequence[0]
+        lastResult = fitter.apply(psfImage, psfShape)
+        lastModel = fitter.getModel()
+        measRecord.set(key, lastResult)
+        # For every other element in the fitting sequence, use the previous fit to initialize the parameters
+        for fitter, key in self.sequence[1:]:
+            initial = fitter.adapt(lastResult, lastModel)
+            lastResult = fitter.apply(psfImage, initial)
+            lastModel = fitter.getModel()
+            measRecord.set(key, lastResult)
+
+
+class ShapeletPsfApproxSingleFrameConfig(lsst.meas.base.SingleFramePluginConfig, ShapeletPsfApproxConfig):
     pass
+
+@lsst.meas.base.register("multifit_ShapeletPsfApprox")
+class ShapeletPsfApproxSingleFramePlugin(lsst.meas.base.SingleFramePlugin, ShapeletPsfApproxMixin):
+    """Minimal subclass of ShapeletPsfApproxMixin to conform to the single-frame measurement API.
+
+    This class simply provides __init__ and measure methods that matched the SingleFramePlugin signatures
+    and delegate to the ShapeletPsfApproxMixin's implmeentations.
+    """
+    ConfigClass = ShapeletPsfApproxSingleFrameConfig
+
+    def __init__(self, config, name, schema, flags, others, metadata):
+        ShapeletPsfApproxMixin.__init__(self, config, name, schema)
+        lsst.meas.base.SingleFramePlugin.__init__(self, config, name, schema, flags, others, metadata)
+
+    def measure(self, measRecord, exposure):
+        ShapeletPsfApproxMixin.measure(self, measRecord, exposure)
+
+class ShapeletPsfApproxForcedConfig(lsst.meas.base.ForcedPluginConfig, ShapeletPsfApproxConfig):
+    pass
+
+@lsst.meas.base.register("multifit_ShapeletPsfApprox")
+class ShapeletPsfApproxForcedPlugin(lsst.meas.base.ForcedPlugin, ShapeletPsfApproxMixin):
+    """Minimal subclass of ShapeletPsfApproxMixin to conform to the forced measurement API.
+
+    This class simply provides __init__ and measure methods that matched the ForcedPlugin signatures
+    and delegate to the ShapeletPsfApproxMixin's implmeentations.
+    """
+    ConfigClass = ShapeletPsfApproxForcedConfig
+
+    def __init__(self, config, name, schemaMapper, flags, others, metadata):
+        ShapeletPsfApproxMixin.__init__(self, config, name, schemaMapper.editOutputSchema())
+        lsst.meas.base.ForcedPlugin.__init__(self, config, name, schemaMapper, flags, others, metadata)
+
+    def measure(self, measRecord, exposure, refRecord, refWcs):
+        ShapeletPsfApproxMixin.measure(self, measRecord, exposure)
---
+++ tests/testShapeletPsfApproxPlugins.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+
+#
+# LSST Data Management System
+# Copyright 2008-2014 LSST Corporation.
+#
+# This product includes software developed by the
+# LSST Project (http://www.lsst.org/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the LSST License Statement and
+# the GNU General Public License along with this program.  If not,
+# see <http://www.lsstcorp.org/LegalNotices/>.
+#
+
+import unittest
+import os
+import glob
+import numpy
+
+import lsst.utils.tests
+import lsst.shapelet
+import lsst.afw.geom.ellipses
+import lsst.afw.table
+import lsst.afw.detection
+import lsst.meas.multifit
+import lsst.meas.base
+
+numpy.random.seed(500)
+
+lsst.pex.logging.Debug("meas.multifit.optimizer.Optimizer", 0)
+lsst.pex.logging.Debug("meas.multifit.optimizer.solveTrustRegion", 0)
+
+class ShapeletPsfApproxPluginsTestCase(lsst.utils.tests.TestCase):
+
+    def setUp(self):
+        self.psfSigma = 2.0
+        self.exposure = lsst.afw.image.ExposureF(41, 41)
+        self.exposure.setPsf(lsst.afw.detection.GaussianPsf(19, 19, self.psfSigma))
+        self.schema = lsst.afw.table.SourceTable.makeMinimalSchema()
+        self.centroidKey = lsst.afw.table.Point2DKey.addFields(self.schema, "centroid", "centroid", "pixels")
+        self.schema.getAliasMap().set("slot_Centroid", "centroid")
+
+    def tearDown(self):
+        del self.exposure
+        del self.schema
+        del self.centroidKey
+
+    def checkResult(self, msf):
+        # Because we're fitting multiple shapelets to a single Gaussian (a single 0th-order shapelet)
+        # we should be able to fit with zero residuals, aside from (single-precision) round-off error.
+        dataImage = self.exposure.getPsf().computeImage()
+        modelImage = dataImage.Factory(dataImage.getBBox())
+        modelImage.getArray()[:,:] *= -1
+        msf.evaluate().addToImage(modelImage)
+        self.assertClose(dataImage.getArray(), modelImage.getArray(), atol=1E-6, plotOnFailure=True)
+
+    def testSingleFrame(self):
+        config = lsst.meas.base.SingleFrameMeasurementTask.ConfigClass()
+        config.slots.centroid = None
+        config.slots.shape = None
+        config.slots.psfFlux = None
+        config.slots.apFlux = None
+        config.slots.instFlux = None
+        config.slots.modelFlux = None
+        config.doReplaceWithNoise = False
+        config.plugins.names = ["multifit_ShapeletPsfApprox"]
+        config.plugins["multifit_ShapeletPsfApprox"].sequence = ["SingleGaussian"]
+        task = lsst.meas.base.SingleFrameMeasurementTask(config=config, schema=self.schema)
+        measCat = lsst.afw.table.SourceCatalog(self.schema)
+        measRecord = measCat.addNew()
+        measRecord.set(self.centroidKey, lsst.afw.geom.Point2D(20.0, 20.0))
+        task.run(measCat, self.exposure)
+        keySingleGaussian = lsst.shapelet.MultiShapeletFunctionKey(
+            self.schema["multifit"]["ShapeletPsfApprox"]["SingleGaussian"]
+            )
+        msfSingleGaussian = measRecord.get(keySingleGaussian)
+        self.assertEqual(len(msfSingleGaussian.getComponents()), 1)
+        self.checkResult(msfSingleGaussian)
+
+    def testForced(self):
+        config = lsst.meas.base.ForcedMeasurementTask.ConfigClass()
+        config.slots.centroid = "base_TransformedCentroid"
+        config.slots.shape = None
+        config.slots.psfFlux = None
+        config.slots.apFlux = None
+        config.slots.instFlux = None
+        config.slots.modelFlux = None
+        config.doReplaceWithNoise = False
+        config.plugins.names = ["base_TransformedCentroid", "multifit_ShapeletPsfApprox"]
+        config.plugins["multifit_ShapeletPsfApprox"].sequence = ["SingleGaussian"]
+        refCat = lsst.afw.table.SourceCatalog(self.schema)
+        refRecord = refCat.addNew()
+        refRecord.set(self.centroidKey, lsst.afw.geom.Point2D(20.0, 20.0))
+        refWcs = self.exposure.getWcs() # same as measurement Wcs
+        task = lsst.meas.base.ForcedMeasurementTask(config=config, refSchema=self.schema)
+        measCat = task.run(self.exposure, refCat, refWcs).sources
+        measRecord = measCat[0]
+        measSchema = measCat.schema
+        keySingleGaussian = lsst.shapelet.MultiShapeletFunctionKey(
+            measSchema["multifit"]["ShapeletPsfApprox"]["SingleGaussian"]
+            )
+        msfSingleGaussian = measRecord.get(keySingleGaussian)
+        self.assertEqual(len(msfSingleGaussian.getComponents()), 1)
+        self.checkResult(msfSingleGaussian)
+
+def suite():
+    """Returns a suite containing all the test cases in this module."""
+
+    lsst.utils.tests.init()
+
+    suites = []
+    suites += unittest.makeSuite(ShapeletPsfApproxPluginsTestCase)
+    suites += unittest.makeSuite(lsst.utils.tests.MemoryTestCase)
+    return unittest.TestSuite(suites)
+
+def run(shouldExit=False):
+    """Run the tests"""
+    lsst.utils.tests.run(suite(), shouldExit)
+
+if __name__ == "__main__":
+    run(True)
