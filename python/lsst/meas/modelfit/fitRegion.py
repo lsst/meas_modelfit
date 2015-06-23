8ff1d2f1067ab4066e65864131556d7201409297
Jim Bosch <jbosch@astro.princeton.edu>
2015-06-23 15:50:03 -0400

    Write new FitRegion code in Python.
    This is quite similar to the more specialized C++ fit region code in
    CModel, and a future refactoring of CModel may delegate to it.  For
    now, we want the flexibility of tweaking it from Python, and we're
    willing to tolerate a small amount of code duplication in the
    meantime.

--- python/lsst/meas/modelfit/fitRegion.py
+++ python/lsst/meas/modelfit/fitRegion.py
@@ -25,33 +25,94 @@ import lsst.pex.config
 import lsst.pipe.base
 import lsst.afw.image
 import lsst.afw.detection
+import lsst.afw.geom.ellipses
 
 __all__ = ("FitRegionConfig", "setupFitRegion")
 
 class FitRegionConfig(lsst.pex.config.Config):
-    """Config class for setupFitRegion() function
+    """Configuration for which pixels to include in a fit.
+
+    See setupFitRegion for more information.
     """
-    nGrow = lsst.pex.config.Field(
+    nGrowFootprint = lsst.pex.config.Field(
         dtype=int,
         default=0,
-        doc="How many pixels to grow detection footprints by"
+        optional=True,
+        doc="How many pixels to grow detection footprints by (may be negative to shrink)."
     )
-    growIsotropic = lsst.pex.config.Field(
-        dtype=bool,
-        default=True,
-        doc="Whether to grow footprints isotropically"
+    nInitialRadii = lsst.pex.config.Field(
+        dtype=float,
+        default=3.0,
+        optional=True,
+        doc="Scale the initial model ellipse by this much."
+    )
+    badMaskPlanes = lsst.pex.config.ListField(
+        dtype=str,
+        default=["SAT", "NO_DATA"],
+        doc="Mask planes that indicate pixels that should not be included in the fit."
     )
 
-def setupFitRegion(config, exposure, source):
-    """Given a SourceRecord (with Footprint) and the Exposure it was detected on,
-    return a new Footprint containing the pixels that should be used in a model
-    fit of the given source.
+    def validate(self):
+        lsst.pex.config.Config.validate(self)
+        if self.nGrowFootprint is None and self.nInitialRadii is None:
+            raise ValueError("At least one of nGrowFootprint and nInitialRadii must be not None.")
 
-    In the future, this might do something fancier than just grow the footprint, such as masking
-    out bad pixels or ORing the detection footprint with an ellipse derived from the source, but
-    we don't anticipate needing any of that for S13.
+
+def setupFitRegion(config, mask=None, detectionFootprint=None, modelEllipse=None, psfMoments=None):
+    """!Return a Footprint containing the pixels that should be used in a model fit.
+
+    The fit region is computed from the union of two regions:
+     - the detection Footprint attached to the SourceRecord, grown by 'nGrowFootprint' pixels;
+     - an initial model ellipse, scaled by 'nInitialRadii', then grown by the RMS size of the PSF.
+
+    If either config parameter is None, that component of the fit region will be ignored entirely
+    (the other component will be used directly, with no union).
+
+    @param[in]  config    FitRegionConfig instance that controls how the region is determined.
+    @param[in]  mask      afw.image.MaskU containing pixel mask information that could cause
+                          pixels to be rejected from the fit (via config.badMaskPlanes).  If
+                          None, config.badMaskPlanes is ignored.
+    @param[in]  detectionFootprint    afw.detection. Footprint that will be grown by config.nGrowFootprint
+                                      before being unioned into the fit region.  If None, the fit region
+                                      will be set by the model ellipse region alone.
+    @param[in]  modelEllipse    afw.geom.ellipses.Ellipse that will be scaled by config.nInitialRadii before
+                                being unioned into the fit region.  If None, the fit region will be set by
+                                the detection footprint region alone.
+    @param[in]  psfMoments      afw.geom.ellipses.BaseCore instance that will be "convolved" (added in
+                                quadrature) with the scaled model ellipse.  If None, the model ellipse
+                                will be left unchanged after scaling.
     """
-    fp = lsst.afw.detection.growFootprint(source.getFootprint(), config.nGrow, config.growIsotropic)
-    fp.clipTo(exposure.getBBox())
-    return fp
+    if config.nGrowFootprint is not None and detectionFootprint is not None:
+        if config.nGrowFootprint < 0:
+            region1 = lsst.afw.detection.shrinkFootprint(detectionFootprint, -config.nGrowFootprint)
+        elif config.nGrowFootprint > 0:
+            region1 = lsst.afw.detection.growFootprint(detectionFootprint, config.nGrowFootprint)
+        else:
+            region1 = detectionFootprint
+    else:
+        region1 = None
+    if config.nInitialRadii is not None and modelEllipse is not None:
+        ellipse = lsst.afw.geom.ellipses.Ellipse(modelEllipse)
+        ellipse.getCore().scale(config.nInitialRadii)
+        if psfMoments is not None:
+            ellipse = ellipse.convolve(lsst.afw.geom.ellipses.Ellipse(psfMoments))
+        region2 = lsst.afw.detection.Footprint(ellipse, mask.getBBox(lsst.afw.image.PARENT))
+    else:
+        region2 = None
+    if region1 and region2:
+        region = lsst.afw.detection.mergeFootprints(region1, region2)
+    elif region1:
+        region = region1
+    elif region2:
+        region = region2
+    else:
+        raise ValueError("Neither detection Footprint nor modelEllipse specified for fit region.")
+    region.clipTo(mask.getBBox(lsst.afw.image.PARENT))
+    if mask is not None and config.badMaskPlanes:
+        badPixelMask = 0x0
+        for plane in config.badMaskPlanes:
+            badPixelMask |= lsst.afw.image.MaskU.getPlaneBitMask(plane);
+        region.intersectMask(mask, badPixelMask)
+    return region
+
 setupFitRegion.ConfigClass = FitRegionConfig
