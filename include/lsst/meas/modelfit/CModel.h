28e72b5c729152ec735783c0c1d6034b166e14c8
Jim Bosch <jbosch@astro.princeton.edu>
2015-11-02 17:45:05 -0500

    Use new semi-empirical prior by default in CModel.

--- include/lsst/meas/modelfit/CModel.h
+++ include/lsst/meas/modelfit/CModel.h
@@ -139,7 +139,7 @@ struct CModelStageControl {
 
     CModelStageControl() :
         profileName("lux"),
-        priorSource("LINEAR"),
+        priorSource("EMPIRICAL"),
         priorName(),
         nComponents(8),
         maxRadius(0),
