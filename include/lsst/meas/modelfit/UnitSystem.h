7739892360c36f4730d7deb290fa35e673f42b72
Russell Owen <rowen@uw.edu>
2018-03-22 12:57:07 -0700

    Run clang-format on UnitSystem.h
    as per reviewer request for internal consistency
    in the placement of & and a desire to use clang-format
    to format my new code.

--- include/lsst/meas/modelfit/UnitSystem.h
+++ include/lsst/meas/modelfit/UnitSystem.h
@@ -33,7 +33,9 @@
 
 #include "lsst/meas/modelfit/common.h"
 
-namespace lsst { namespace meas { namespace modelfit {
+namespace lsst {
+namespace meas {
+namespace modelfit {
 
 /**
  *  @brief A simple struct that combines a Wcs and a Calib.
@@ -54,19 +56,17 @@ struct UnitSystem {
     UnitSystem(afw::geom::SpherePoint const& position, Scalar mag);
 
     /// Construct a UnitSystem from a give Wcs and Calib
-    UnitSystem(PTR(afw::geom::SkyWcs const) wcs_, PTR(afw::image::Calib const) calib_) :
-        wcs(wcs_), calib(validateCalib(calib_))
-    {}
+    UnitSystem(PTR(afw::geom::SkyWcs const) wcs_, PTR(afw::image::Calib const) calib_)
+            : wcs(wcs_), calib(validateCalib(calib_)) {}
 
     /// Construct a UnitSystem by extracting the Wcs and Calib from an Exposure (implicit)
     template <typename T>
-    UnitSystem(afw::image::Exposure<T> const & exposure) :
-        wcs(exposure.getWcs()), calib(validateCalib(exposure.getCalib()))
-    {}
+    UnitSystem(afw::image::Exposure<T> const& exposure)
+            : wcs(exposure.getWcs()), calib(validateCalib(exposure.getCalib())) {}
 
 private:
     std::shared_ptr<const lsst::afw::image::Calib> validateCalib(
-        std::shared_ptr<const lsst::afw::image::Calib> calib_);
+            std::shared_ptr<const lsst::afw::image::Calib> calib_);
     static std::shared_ptr<const lsst::afw::image::Calib> getDefaultCalib();
 };
 
@@ -78,7 +78,6 @@ private:
  *  AffineTransform and the photometric transform as a simple scaling.
  */
 struct LocalUnitTransform {
-
     /// Maps source pixel coordinates to destination pixel coordinates
     afw::geom::AffineTransform geometric;
 
@@ -88,17 +87,15 @@ struct LocalUnitTransform {
     /// Multiply source surface brightnesses by this to get destination surface brightnesses
     double sb;
 
-    LocalUnitTransform(
-        afw::geom::Point2D const & sourcePixel,
-        UnitSystem const & source,
-        UnitSystem const & destination
-    );
+    LocalUnitTransform(afw::geom::Point2D const& sourcePixel, UnitSystem const& source,
+                       UnitSystem const& destination);
 
     /// Construct an identity transform for both geometry and flux.
     LocalUnitTransform() : geometric(), flux(1.0), sb(1.0) {}
-
 };
 
-}}} // namespace lsst::meas::modelfit
+}  // namespace modelfit
+}  // namespace meas
+}  // namespace lsst
 
-#endif // !LSST_MEAS_MODELFIT_UnitSystem_h_INCLUDED
+#endif  // !LSST_MEAS_MODELFIT_UnitSystem_h_INCLUDED
